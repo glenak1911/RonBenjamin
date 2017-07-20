495b690c816d42a82fea9cf617ae90bffacaf3a7
Glen Knight <glenak1911@gmail.com>
2017-07-20 01:29:08 -0400

    Updated link on 404 page

--- 404.php
+++ 404.php
@@ -2,8 +2,8 @@
 	<div class="error_page center">
 		<h2 class="category-title center-title">404 Page Not Found</h2>
 		<img class="error_img" src="<?php bloginfo('template_directory'); ?>/img/404cat.jpg">
-		<p>Well ain't this embarassing! Apparently this link doesn't exist.</p>
-	  <p> Please visit our <a title="Our Site" href="https://yannipaul.com">Home Page</a> to start fresh.</p>
+		<p>Well isn't this embarassing! Apparently this link doesn't exist.</p>
+	  <p> Please visit our <a title="Our Site" href="<?php bloginfo( 'url' );?>">Home Page</a> to start fresh.</p>
 		<p>We'll do better next time.</p>
 		<p>Promise!</p>
 	</div>
