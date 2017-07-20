40e0c034d18998c51ab9c6f2fe1e7cb7ec34304f
Glen Knight <glenak1911@gmail.com>
2017-07-20 00:59:57 -0400

    Updated styling on footer and removed comments on main page

--- footer.php
+++ footer.php
@@ -4,43 +4,22 @@
 
 <footer class="blog-footer">
     <div class="row">
-      <div class="col-md-4">
-      </div>
-      <div class="col-md-4">
+      <div class="col-md-4 col-md-offset-4">
         <ol class="socialMedia">
           <li><a target="_blank" class="fa fa-facebook socialMediaItem" href="<?php echo get_option('facebook'); ?>"></a></li>
           <li><a target="_blank" class="fa fa-twitter socialMediaItem" href="<?php echo get_option('twitter'); ?>"></a></li>
           <li><a target="_blank" class="fa fa-google-plus socialMediaItem" href="<?php echo get_option('google'); ?>"></a></li>
         </ol>
       </div>
-      <div class="col-md-4">
-      </div>
     </div>
     <div class="row">
-      <div class="col-md-4">
-      </div>
-      <div class="col-md-4">
-          <!--<div class="searchBox"><?php get_search_form(); ?></div>-->
-      </div>
-      <div class="col-md-4">
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-md-4">
-      </div>
-      <div class="col-md-4">
+      <div class="col-md-4 col-md-offset-4">
           <p>Paid for by The Committee to Elect Ronald Benjamin</p>
       </div>
-      <div class="col-md-4">
-      </div>
     </div>
     <div class="row">
-      <div class="col-md-4">
-      </div>
-      <div class="col-md-4">
+      <div class="col-md-4 col-md-offset-4">
         <p><a class="credits" target="_blank" href="https://knightglen.com">KnightVision</a> Design</p>
-      </div>
-      <div class="col-md-4">
       </div>
     </div>
 </footer>
--- page.php
+++ page.php
@@ -3,7 +3,6 @@
 		<div class="col-sm-12">
 			<?php while ( have_posts() ) : the_post(); ?>
 				<?php get_template_part( 'content', 'page' ); ?>
-				<?php comments_template( '', true ); ?>
 			<?php endwhile; // end of the loop. ?>
 
 		</div> <!-- /.col -->
